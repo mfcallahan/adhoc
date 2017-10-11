ae8b51d2e2545e01ac2ace75914489ffc768c6ea
Matthew Callahan <matthew.callahan@outlook.com>
2017-10-11 15:06:22 -0700

    Auto stash before merge of "master" and "matt-dev"

---
+++ src/Python/reddit/reddit_bot_config.py
@@ -0,0 +1,15 @@
+# bot account
+username = "b0t_________________"
+password = "thisISmyb0t!"
+client_id = "vlAH9CZEajHTBA"
+client_secret = "jVTB_0iLhkuzZ_e4Q44H8KgB1oA"
+
+
+# -----------------------------
+# other accounts
+# 
+# DONT_BE_RETARDED_
+# beard_the_fock_up
+# HueyLewisAndTheJews_
+# 
+# -----------------------------
