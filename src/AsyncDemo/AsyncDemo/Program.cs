d5214b7298251d501058ae2b678dfae29f6736db
Matthew Callahan <matthew.callahan@outlook.com>
2018-06-03 14:57:53 -0700

    sync and async demos

--- src/AsyncDemo/AsyncDemo/Program.cs
+++ src/AsyncDemo/AsyncDemo/Program.cs
@@ -10,15 +10,65 @@ namespace AsyncDemo
         // https://stackoverflow.com/questions/13002507/how-can-i-call-async-go-method-in-for-example-main
         // https://stackoverflow.com/questions/9343594/how-to-call-asynchronous-method-from-synchronous-method-in-c
 
+        // Wait time in seconds for SampleDataLayer methods
+        const int _getDelayedResponseWaitTime = 4;
+        const int _simulateLongProcessWaitTime = 3;
+
         static async Task Main()
         {
-            Console.WriteLine("App start.");
+            Stopwatch s = new Stopwatch();
 
-            // start long running asynchronous data fethcing work
-            Task<string> dataA = SampleDataLayer.GetDelayedResponse(8);
-            Task<string> dataB = SampleDataLayer.SimulateLongProcess(4);
+            // RunDemoSynchronous() will call three methods in SampleDataLayer synchronously
+            s.Start();
+            RunDemoSynchronous();
+            s.Stop();
 
-            // start synchronous work which doesn't need to wait for dataA or dataB
+            Console.WriteLine("RunDemoSynchronous() complete. Elapsed seconds: " + s.Elapsed.TotalSeconds + Environment.NewLine);
+
+            // RunDemoAsynchronous() will call three methods in SampleDataLayer asynchronously
+            s.Restart();
+            await RunDemoAsynchronous();
+            s.Stop();
+            Console.WriteLine("RunDemoAsynchronous() complete. Elapsed seconds: " + s.Elapsed.TotalSeconds + Environment.NewLine);
+
+            Console.WriteLine("Demo complete.");
+            Console.ReadLine();
+        }
+
+        static void RunDemoSynchronous()
+        {
+            Console.WriteLine("RunDemoSynchronous() start.");
+
+            // execute long and short running methods synchronously
+            string dataA = SampleDataLayer.GetDelayedResponse(_getDelayedResponseWaitTime);
+            string dataB = SampleDataLayer.SimulateLongProcess(_simulateLongProcessWaitTime);
+            int dataC = SampleDataLayer.Foo();
+
+            SampleData sample = new SampleData()
+            {
+                A = dataA,
+                B = dataB,
+                C = dataC
+            };
+
+            Console.WriteLine("SampleData object 'sample' created:");
+            Console.WriteLine("sample.A = {0}", sample.A);
+            Console.WriteLine("sample.B = {0}", sample.B);
+            Console.WriteLine("sample.C = {0}", sample.C);
+        }
+
+        async static Task RunDemoAsynchronous()
+        {
+            Console.WriteLine("RunDemoAsynchronous() start.");
+
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            // start long running asynchronous methods
+            Task<string> dataA = SampleDataLayer.GetDelayedResponseAsync(_getDelayedResponseWaitTime);
+            Task<string> dataB = SampleDataLayer.SimulateLongProcessAsync(_simulateLongProcessWaitTime);
+
+            // start synchronous method which doesn't need to wait for dataA or dataB
             int dataC = SampleDataLayer.Foo();
 
             // now call await on the tasks
@@ -30,13 +80,10 @@ namespace AsyncDemo
                 C = dataC
             };
 
-            Console.WriteLine("SampleData object created:");
-            Console.WriteLine("response.A = {0}", sample.A);
-            Console.WriteLine("response.B = {0}", sample.B);
-            Console.WriteLine("response.C = {0}", sample.C);
-
-            Console.WriteLine("Complete.");
-            Console.ReadLine();
-        }        
+            Console.WriteLine("SampleData object 'sample' created:");
+            Console.WriteLine("sample.A = {0}", sample.A);
+            Console.WriteLine("sample.B = {0}", sample.B);
+            Console.WriteLine("sample.C = {0}", sample.C);
+        }
     }
 }
---
+++ src/AsyncDemo/AsyncDemo/SampleDataLayer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo
+{
+    public static class SampleDataLayer
+    {
+        const string _apiUrl = "http://mfcallahan.com/api/GetDelayedResponse?waitSeconds=";
+
+        // synchronous method to simulate a long running HTTP request
+        public static string GetDelayedResponse(int seconds)
+        {
+            using (WebClient client = new WebClient())
+            {
+                Console.WriteLine("Starting /api/GetDelayedResponse");
+                var message = client.DownloadString(string.Concat(_apiUrl, seconds));
+                Console.WriteLine("/api/GetDelayedResponse complete.");
+
+                return message.Trim('"');
+            }
+        }
+
+        // async method to simulate a long running HTTP request
+        public async static Task<string> GetDelayedResponseAsync(int seconds)
+        {
+            using (WebClient client = new WebClient())
+            {
+                Console.WriteLine("Starting async /api/GetDelayedResponse");
+                var message =  await client.DownloadStringTaskAsync(string.Concat(_apiUrl, seconds));
+                Console.WriteLine("async /api/GetDelayedResponse complete.");
+
+                return message.Trim('"');
+            }            
+        }
+
+        // synchronous method to simualte long running work in application code
+        public static string SimulateLongProcess(int seconds)
+        {
+            Console.WriteLine("Starting SimulateLongProcess()...");
+            Thread.Sleep(seconds * 1000);
+            string data = "Hello, world!";
+            Console.WriteLine("SimulateLongProcess() complete.");
+
+            return data;
+        }
+
+        // async method to simualte long running work in application code
+        public async static Task<string> SimulateLongProcessAsync(int seconds)
+        {
+            Console.WriteLine("Starting SimulateLongProcessAsync()...");
+            await Task.Delay(seconds * 1000);
+            string data = "Hello, world!";
+            Console.WriteLine("SimulateLongProcessAsync() complete.");
+
+            return data;
+        }
+
+        // synchronous methods to simulate short running work in application code
+        public static int Foo()
+        {
+            Console.WriteLine("Starting Foo()...");
+            int n = Bar(10);
+            Console.WriteLine("Foo() complete.");
+            return n;
+        }
+
+        static int Bar(int n)
+        {
+            return 100 * n;
+        }
+    }
+
+    public class SampleData
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+        public int C { get; set; }
+    }
+}
