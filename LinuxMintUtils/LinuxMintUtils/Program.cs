687410fe040b76139ade37e25c05ab61cb38e462
Matt Callahan <matthew.callahan@outlook.com>
2020-08-27 12:41:10 -0700

    add new empty console app sln

---
+++ LinuxMintUtils/LinuxMintUtils/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinuxMintUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
