f638a5d3a625f6002a78a6c19bb5aef131cc3214
Matt Callahan <matthew.callahan@outlook.com>
2020-08-31 08:08:49 -0700

    moving to new repo

---
+++ LinuxMintUtils/FileUtils/FolderCleanup/CleanupDir.cs
@@ -0,0 +1,8 @@
+namespace FolderCleanup
+{
+    public class CleanupDir
+    {
+        public int DeleteFilesOlderThanDays { get; set; }
+        public string Dir { get; set; }
+    }
+}
--- LinuxMintUtils/FileUtils/FolderCleanup/Program.cs
+++ LinuxMintUtils/FileUtils/FolderCleanup/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security;
+using Microsoft.Extensions.Configuration;
 
 namespace FolderCleanup
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var appSettings = GetAppConfigs();
+            var errors = new List<string>();
+
+            // call DeleteFiles() for each dir
+            foreach (var dir in appSettings)
+            {
+                
+            }
+
+            // handle errors..
+            if (errors.Count > 0)
+            {
+                foreach (var error in errors)
+                {
+                    Console.WriteLine(error);
+                }
+
+                Console.ReadLine();
+            }
+            
+        }
+
+        private static List<string> DeleteFiles(string dir, int days)
+        {
+            var files = Directory.EnumerateFiles(dir, "*.*", SearchOption.AllDirectories);
+            var notDeleted = new List<string>();
+            
+            foreach (var file in files)
+            {
+                var fi = new FileInfo(file);
+
+                if (fi.CreationTime >= DateTime.Now.AddDays(days))
+                {
+                    continue;
+                }
+
+                try
+                {
+                    fi.Delete();
+                }
+                catch(IOException ex)
+                {
+                    notDeleted.Add($"File {fi.Name} can't be deleted: file is in use.");
+                }
+                catch(SecurityException ex)
+                {
+                    notDeleted.Add($"File {fi.Name} can't be deleted: insufficient permission.");
+                }
+                catch(UnauthorizedAccessException ex)
+                {
+                    notDeleted.Add($"File {fi.Name} can't be deleted: access denied.");
+                }
+            }
+
+            return notDeleted;
+        }
+        
+        private static IConfigurationSection GetAppConfigs()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            return configuration.GetSection("Settings");
         }
     }
 }
