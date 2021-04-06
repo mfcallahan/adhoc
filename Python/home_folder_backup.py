cf4ed91eb7ee99b328e5757f30f432d5b4ad503b
Matt Callahan <matthew.callahan@outlook.com>
2021-04-06 16:00:23 -0700

    folder backup deployed

--- Python/home_folder_backup.py
+++ Python/home_folder_backup.py
@@ -1,21 +1,21 @@
 # Copy all files from a specified source folder in the Home directory to a specified destination folder for backup
 # purposes. If a file exists in the destination folder but not the source folder, it is deleted.
 
+# Deploy:
+# cp -u home_folder_backup.py /home/matt/Scripts
+
 import os
-import logging
 import glob
+import logging
 import datetime
-import fnmatch
 from datetime import datetime
 from types import SimpleNamespace
 from shutil import copyfile
 from pathlib import Path
 
-backupPath = '/mnt/XtraDisk/OneDrive/Mint_Backup'
-
-backupFolders = [
-    SimpleNamespace(sourcePath = '/home/matt/Documents', errors = []),
-    SimpleNamespace(sourcePath = '/home/matt/Pictures', errors = [])
+foldersToBackup = [
+    SimpleNamespace(sourcePath = '/home/matt/Documents', backupPath = '/mnt/XtraDisk/OneDrive/Mint_Backup/Documents', errors = []),
+    SimpleNamespace(sourcePath = '/home/matt/Pictures', backupPath = '/mnt/XtraDisk/OneDrive/Mint_Backup/Pictures',errors = [])
 ]
 
 def main():
@@ -23,7 +23,7 @@ def main():
     logFileName = f'{os.path.splitext(os.path.basename(__file__))[0]}.log'
     configureLogger(logFileName)
 
-    for folder in backupFolders:
+    for folder in foldersToBackup:
         syncFolders(folder)
 
         # log each error to file
@@ -31,49 +31,49 @@ def main():
 
     logging.info(f'Folder backup complete at {datetime.now().strftime("%d-%m-%Y %H:%M:%S")}.')
 
-# configure logging to write to file, overwriting the log file contents each time
 def configureLogger(logFileName):
     logging.basicConfig(
         filename=logFileName,
-        filemode='w',
+        filemode='w', # overwrite the log file contents each time
         format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
         datefmt='%m/%d/%Y %H:%M:%S',
         level=logging.DEBUG
     )
  
-def syncFolders(backupFolder):
-    sourceFiles = getAllFilesInFolder(backupFolder.sourcePath)
-    destFiles = getAllFilesInFolder(backupFolder.destPath)
+def syncFolders(folderToBackup):
+    sourceFiles = getAllFilesInFolder(folderToBackup.sourcePath)
+    destFiles = getAllFilesInFolder(folderToBackup.backupPath)
 
     # delete any files in the destination folder which do not exist in the source folder
     for file in destFiles:
-        if (sourceFiles.count(file.replace(backupFolder.destPath, '~')) == 0):
+        if (sourceFiles.count(file.replace(folderToBackup.backupPath, folderToBackup.sourcePath)) == 0):
             try:
                 os.remove(file)
             except OSError as e:
-                backupFolder.errors.append(f'File "{file}" - {e.strerror}')
-            
+                folderToBackup.errors.append(f'File "{file}" - {e.strerror}')
+
     # delete any empty folders left inside the destination folder
     try:
-        deleteEmptyFolders(backupFolder.destPath)
+        deleteEmptyFolders(folderToBackup.backupPath)
     except OSError as e:
-        backupFolder.errors.append(f'deleteEmptyFolders({backupFolder.destPath}) error: {e.strerror}')
-    
+        folderToBackup.errors.append(f'Error deleting empty folders: ({folderToBackup.backupPath}) error: {e.strerror}')
+
     # copy all files from source folder to destination folder
     for sourceFile in sourceFiles:
-        destFile = os.path.join(backupFolder.destPath , os.path.basename(os.path.normpath(backupFolder.sourcePath)))
+        destFile = os.path.join(folderToBackup.backupPath, sourceFile.replace(folderToBackup.sourcePath, '').lstrip('/'))
         try:
             os.makedirs(os.path.dirname(destFile), exist_ok=True)
             copyfile(sourceFile, destFile)
         except OSError as e:
-            backupFolder.errors.append(f'Error copying "{sourceFile}" to "{destFile}" - {e.strerror}')
+            folderToBackup.errors.append(f'Error copying "{sourceFile}" to "{destFile}" - {e.strerror}')
 
 def getAllFilesInFolder(folder):
-    files = []
-    for file in Path(folder).glob('**/*.*'):
-        files.append(str(file))
+    allFiles = []
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            allFiles.append(os.path.abspath(os.path.join(root, file)))
     
-    return files
+    return allFiles
 
 def deleteEmptyFolders(folder):
     for root, dirs, files in os.walk(folder, topdown = False):
